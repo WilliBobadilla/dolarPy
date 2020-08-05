4cff15c4e8cb27c4e00551911b18404b08d7f2ec
WilliBobadilla <willi1997.1@gmail.com>
2020-08-05 19:55:19 -0400

    funciona pero falta agregar boton para anhadir app

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,15 @@
 
+alert("sdfasd")
+
 if('serviceWorker' in navigator){
+  console.log("serviceworker")
+  alert("service worker entro en el if ")
     try {
-      navigator.serviceWorker.register('/serviceWorker.js');
+      navigator.serviceWorker.register('/js/serviceWorker.js');
       console.log("Service Worker Registered");
+      alert("serviceworker");
     } catch (error) {
       console.log("Service Worker Registration Failed");
+      alert("no registrado")
     }
-  }
-
+  } 
